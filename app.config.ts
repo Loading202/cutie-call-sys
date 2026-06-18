6e60a328f4c0519750bcee5121e9b7e6e18a28eb
Loading202 <88673554+Loading202@users.noreply.github.com>
2026-06-18 19:30:43 +0000

    .

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from "@tanstack/react-start/config";
-import { fileURLToPath } from "url";
-import path from "path";
 
 export default defineConfig({
   server: {
@@ -9,7 +7,7 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+        "@": "/opt/buildhome/repo/src",
       },
     },
   },
